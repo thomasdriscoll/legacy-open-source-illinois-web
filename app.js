87e563ddcb00205889bd9b1dd83e4b2f26da2fa5
Thomas <thomasdriscoll98@gmail.com>
2019-04-21 09:41:35 -0500

    Working Node.js

--- app.js
+++ app.js
@@ -1,45 +1,24 @@
-// const http = require('http');
-// const url = require('url');
-// const fs = require('fs');
-//
-// const hostname = '127.0.0.1';
-// const port = 3000;
-//
-// const server = http.createServer((req, res) => {
-//   var q = url.parse(req.url, true);
-//   var filename = '.' + q.pathname;
-//   if(filename='.'){
-//     filename += '/index.html'
-//   }
-//   fs.readFile(filename, (err, data) => {
-//     if(err){
-//       res.writeHead(404, {'Content-Type':'text/html'});
-//       return res.end('404 Not Found');
-//     }
-//   res.writeHead(200, {'Content-Type': 'text/html'});
-//   res.write(data);
-//   res.end();
-//   });
-// }).listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`)});
-
+// Load required modules
 const express = require('express');
 const url = require('url');
 const app = express();
 const port = 3000;
-
+//Specify where static are located (css, images, javascripts)
 app.use(express.static('public'));
-app.get('/', (req, res) => {
-  var filename = './index.html';
-  fs.readFile(filename, (err, data) => {
-    if(err){
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end('404 Not Found');
-    }
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
-  });
-});
+//Specify view engine -- i.e. templating engine
+app.engine('pug', require('pug').__express)
+app.set('view engine', 'pug');  //Pug was chosen as it is the default for Node.js AND there's lots of documentation
 
-app.listen(port, () => console.log('Listening on port ${port}!'));
+//I know there's a better way to do the templating functions but not there yet
+//Render home page
+app.get('/', (req, res) => {
+  res.render('home', {header: 'Home'});
+});
+//Render login page
+app.get('/join', (req,res) =>{
+  res.render('join', {header: 'Sign Up'})
+});
+app.get('/members', (req,res) =>{
+  res.render('members', {header: 'Members'})
+});
+app.listen(port, () => console.log(`Listening on port ${port}`));
